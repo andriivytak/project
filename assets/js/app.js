e641ed5991dea3788280736ed96f1165cbe58074
Andrii <andrii.vytak@gmail.com>
2021-02-17 17:48:10 +0200

    cycle java script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,34 @@
+"use strict";
+
+let numberOfFilms = prompt('Скільки фільмів ви подивилися?'),
+
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+
+
+for (let i = 1; i <= 2; i++) {
+    let a = prompt("Який фільм подивилися?"),
+        b = prompt("Яку оцінку поставите?");
+
+    if (a != null && b != null && a != '' && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
 
 
 
 
-let contsasn = '123';
+}
 
 
-console.log(contsasn);
 
-
-const kelbasa = 'kekekekekekekekek';
+console.log(personalMovieDB);
